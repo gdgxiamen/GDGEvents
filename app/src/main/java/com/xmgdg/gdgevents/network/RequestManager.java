19e9ef067fe153094256e63084d76d8054b51de2
YeX <yexiang@outlook.com>
2015-05-17 09:38:46 +0800

    增加volley和gson库，测试提交

---
+++ app/src/main/java/com/xmgdg/gdgevents/network/RequestManager.java
@@ -0,0 +1,14 @@
+package com.xmgdg.gdgevents.network;
+
+/**
+ * Created by ye on 15/5/17.
+ */
+public class RequestManager {
+
+    private static final String API_HOST = "https://developers.google.com/events/feed/json?group=";
+    private static final String ID_XIAMEN = "110967416369300099369";
+    private static final String STARTTIME_XIAMEN = "1356998399";
+
+    public static final String API_XIAMEN = API_HOST + ID_XIAMEN + "&start=" + STARTTIME_XIAMEN;
+
+}
